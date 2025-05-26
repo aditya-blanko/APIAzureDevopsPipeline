76928468e4a28bb100ec5d310bcf9980dbf5bcd6
Aditya singhal <singhaladitya703@gmail.com>
2025-05-26 11:50:29 +0530

    Enable Swagger middleware in all environments
    Modified the application to use Swagger and Swagger UI unconditionally, removing the environment check. This change enhances API documentation accessibility by making it available in all environments, not just during development.

--- APIAzureDevopsPipeline/Program.cs
+++ APIAzureDevopsPipeline/Program.cs
@@ -19,9 +19,11 @@ namespace APIAzureDevopsPipeline
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
